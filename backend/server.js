e28131e1e450c431236983976c07287440d8de15
athuliyaps <athuliyaps7575@gmail.com>
2026-08-29 08:05:00 +0530

    backend:Database and server connected

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const sequelize = require('./src/config/database')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/',(req,res)=>{
+    res.json({message:"HireBoard API is running"})
+})
+
+const PORT = process.env.PORT || 5000
+
+const server = async()=>{
+    try{
+  await sequelize.authenticate()
+  console.log(`DB ${process.env.DB_NAME} connected sucessfully`);
+  
+  app.listen(PORT,()=>{
+    console.log(`Server running in port ${PORT}`);
+  })
+
+    }catch(err){
+     console.error('Unable to connect to the databse', err.message);
+     
+    }
+}
+server()
